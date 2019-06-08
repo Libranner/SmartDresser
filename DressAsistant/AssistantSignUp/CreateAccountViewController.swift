9bec9a8c38440d5772571dc7c4b7de59b098b237
libranner@saestech.com <libranner@saestech.com>
2019-06-08 23:27:51 +0200

    Create user views

--- DressAsistant/AssistantSignUp/CreateAccountViewController.swift
+++ DressAsistant/AssistantSignUp/CreateAccountViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class CreateAccountViewController: UIViewController {
+class CreateAccountViewController: BaseViewController {
   private let alertSheetTakePhotoStringId = "take-photo"
-  private let alertSheetChoosePhotoStringId = "choose-photo"
+  private let alertSheetChoosePhotoStringId = "choose-gallery-photo"
+  private let alertSheetMessageSelectPhotoStringId = "select-photo"
+  private let alertSheetCancelStringId = "cancel-action"
   
   @IBOutlet weak var profilePicture: UIImageView!
   @IBOutlet weak var nicknameTextfield: UITextField!
   
+  @IBOutlet weak var selectPhotoButton: UIButton!
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -26,9 +29,13 @@ class CreateAccountViewController: UIViewController {
   }
   
   @IBAction func selectProfilePhoto(_ sender: Any) {
-    let alertVC = UIAlertController(title: "", message: "", preferredStyle: .actionSheet)
+    
     let takePhotoString = NSLocalizedString(alertSheetTakePhotoStringId, comment: "")
     let choosePhotoString = NSLocalizedString(alertSheetChoosePhotoStringId, comment: "")
+    let selectPhotoString = NSLocalizedString(alertSheetMessageSelectPhotoStringId, comment: "")
+    let cancelActionString = NSLocalizedString(alertSheetCancelStringId, comment: "")
+    
+    let alertVC = UIAlertController(title: "", message: selectPhotoString, preferredStyle: .actionSheet)
     
     let takePhotoAction = UIAlertAction(title: takePhotoString, style: .default) { [weak self] _ in
       self?.takePhoto()
@@ -38,8 +45,11 @@ class CreateAccountViewController: UIViewController {
       self?.selectPhotoFromGallery()
     }
     
+    let cancelAction = UIAlertAction(title: cancelActionString, style: .destructive)
+    
     alertVC.addAction(takePhotoAction)
     alertVC.addAction(choosePhotoAction)
+    alertVC.addAction(cancelAction)
     present(alertVC, animated: true)
   }
 }
@@ -64,6 +74,7 @@ extension CreateAccountViewController: UINavigationControllerDelegate, UIImagePi
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
     let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
     profilePicture.image = pickedImage
+    selectPhotoButton.alpha = 0
     self.dismiss(animated: true, completion: nil)
   }
 }
